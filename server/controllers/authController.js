cb6def3003e7a1b96c1f7b981daebb957d22a620
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-28 08:02:06 +0900

    refactoring emailjs code store helpers folder

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,6 +2,9 @@ const User = require('../models/user');
 const AWS = require('aws-sdk');
 const jwt = require('jsonwebtoken');
 
+// helpers
+const { registerEnailParams } = require('../helpers/email');
+
 // @document  https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/ses-examples-sending-email.html
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -14,16 +17,19 @@ const ses = new AWS.SES({ apiVersion: '2010-12-01' });
 // @Method     POST
 // @API ROUTE  /api/auth/register
 // @Desc       Register new User & send Email
-exports.register = (req, res) => {
+exports.register = async (req, res) => {
   console.log('register req.body', req.body);
   const { name, email, password } = req.body;
 
   // check if user exists or not
-  User.findOne({ email }).exec(function (err, user) {
-    console.log('exec/error', err);
-    console.log('exec/user', user);
-
-    if (user) return res.status(400).json({ error: 'Email is taken' });
+  try {
+    const user = await User.findOne({ email });
+    // user have been registerd
+    console.log('user', user);
+    if (user)
+      return res
+        .status(400)
+        .json({ message: 'Email is taken', status: 'failed' });
 
     // generate token with user email and _password
     const token = jwt.sign(
@@ -35,43 +41,72 @@ exports.register = (req, res) => {
     );
 
     // Send email
-    const params = {
-      Source: process.env.EMAIL_FROM,
-      Destination: {
-        ToAddresses: [email]
-      },
-      ReplyToAddresses: [
-        process.env.EMAIL_TO
-        /* more items */
-      ],
-      Message: {
-        /* required */
-        Body: {
-          /* required */
-          Html: {
-            Charset: 'UTF-8',
-            Data: `<html>
-                    <h1>Verify your email address</h1>
-                    <p>Please use the following link complete your registration</p>
-                    <p>${process.env.CLIENT_URL}/auth/activate/${token}}</p>
-                  </html>`
-          }
-        },
-        Subject: {
-          Charset: 'UTF-8',
-          Data: 'Complete your registration'
-        }
-      }
-    };
+    const params = registerEnailParams(email, token);
+
     const sendEmailOnRegister = ses.sendEmail(params).promise();
+
     sendEmailOnRegister
       .then((data) => {
         console.log('Email submitted to SES', data);
-        res.send('Email sent');
+        res.status(200).json({
+          message: `Email has been sent to ${email}, Follow the instruction to complete your registration`,
+          status: 'success'
+        });
       })
       .catch((error) => {
         console.log('SES email on register', error);
-        res.send('Email failed');
+        res.status(401).json({
+          errors: error,
+          message: 'We could not verify your email, please try again.',
+          status: 'failed'
+        });
       });
-  });
+  } catch (error) {
+    res.status(500).json({
+      errors: error,
+      message: 'Server error at finding user infomation.',
+      status: 'failed'
+    });
+  }
 };
+
+// User.findOne({ email }).exec(function (err, user) {
+//   console.log('exec/error', err);
+//   console.log('exec/user', user);
+
+//   // user have been registerd
+//   if (user)
+//     return res
+//       .status(400)
+//       .json({ message: 'Email is taken', status: 'failed' });
+
+//   // generate token with user email and _password
+//   const token = jwt.sign(
+//     { name, email, password },
+//     process.env.JWT_ACCOUNT_ACTIVATION,
+//     {
+//       expiresIn: '10m'
+//     }
+//   );
+
+//   // Send email
+//   const params = registerEnailParams(email, token);
+
+//   const sendEmailOnRegister = ses.sendEmail(params).promise();
+//   sendEmailOnRegister
+//     .then((data) => {
+//       console.log('Email submitted to SES', data);
+//       res.status(200).json({
+//         message: `Email has been sent to ${email}, Follow the instruction to complete your registration`,
+//         status: 'success'
+//       });
+//     })
+//     .catch((error) => {
+//       console.log('SES email on register', error);
+//       res.status(401).json({
+//         errors: error,
+//         message: `Email has been sent to ${email}, Follow the instruction to complete your registration`,
+//         status: 'failed'
+//       });
+//     });
+// });
---
+++ server/helpers/email.js
@@ -0,0 +1,30 @@
+exports.registerEnailParams = (email, token) => {
+  return {
+    Source: process.env.EMAIL_FROM,
+    Destination: {
+      ToAddresses: [email]
+    },
+    ReplyToAddresses: [
+      process.env.EMAIL_TO
+      /* more items */
+    ],
+    Message: {
+      /* required */
+      Body: {
+        /* required */
+        Html: {
+          Charset: 'UTF-8',
+          Data: `<html>
+                    <h1>Verify your email address</h1>
+                    <p>Please use the following link complete your registration</p>
+                    <p>${process.env.CLIENT_URL}/auth/activate/${token}}</p>
+                  </html>`
+        }
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'Complete your registration'
+      }
+    }
+  };
+};
