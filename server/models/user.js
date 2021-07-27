c7a0e3f011f232067a24863061d2f63475509ca9
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-07-27 15:28:41 +0900

    usermodel created

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema(
   {
@@ -61,15 +61,29 @@ UserSchema.virtual('password')
   });
 
 UserSchema.methods = {
-  encryptPassword: function (password) {
+  // @desc  compare detabase password & incomming password
+  authenticate: async function (plainPassword) {
+    const isMatch = await bcrypt.compare(plainPassword, this.hashed_password);
+    console.log('isMatch', isMatch);
+    return isMatch;
+  },
+  encryptPassword: async function (password) {
     if (!password) return '';
     try {
-      return crypto
-        .createHash('sha1', this.salt)
-        .update(password)
-        .digest('hex');
+      const hashed_password = await bcrypt.hash(password, this.salt);
+      console.log('hashed_password', hashed_password);
+      return hashed_password;
+      return;
     } catch (error) {
       return '';
     }
+  },
+  makeSalt: async function () {
+    const salt = await bcrypt.genSalt(10);
+    console.log('salt', salt);
+    return salt;
   }
 };
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
