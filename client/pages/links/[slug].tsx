ab147786598d18932f05dae42e1d435cc4960b50
春日崇希 <kasugatakashinozomi@kasugatakashinozominoMacBook-Pro.local>
2021-08-04 10:17:25 +0900

    category detail function implemented

--- client/pages/links/[slug].tsx
+++ client/pages/links/[slug].tsx
@@ -76,33 +76,44 @@ const Links: FC<LinksProps> = ({
   const [allLinks, setAllLinks] = useState<LinksRes>(links);
   return (
     <Header>
-      <div className='md:grid md:grid-cols-4 md:gap-4'>
+      <div className='flex flex-col-reverse md:grid md:grid-cols-4 md:gap-4'>
         <div className='md:col-span-3'>
-          <h1>{category.name}</h1>
+          <h1 className='md:text-3xl mb-3'>{category.name} - URL/Links</h1>
+          <div className='p-5 bg-gray-200 mb-5'>
+            {ReactHtmlParser(category.content)}
+          </div>
           <div>
             {allLinks.map((link, index) => {
               return (
-                <div key={index}>
-                  <div>
-                    <a
-                      href={link.url}
-                      target='_blank'
-                      rel='noopener noreferrer'>
-                      <h5>{link.title}</h5>
-                      <h6>{link.url}</h6>
-                    </a>
+                <div key={index} className='bg-blue-200 p-5 rounded'>
+                  <div className='md:flex md:justify-between md:items-center mb-5'>
+                    <div className='md:w-8/12'>
+                      <a
+                        className='text-blue-600 hover:text-white hover:underline'
+                        href={link.url}
+                        target='_blank'
+                        rel='noopener noreferrer'>
+                        <h5>{link.title}</h5>
+                        <h6>{link.url}</h6>
+                      </a>
+                    </div>
+                    <div className='md:w-3/12'>
+                      <span>
+                        {moment(link.createdAt).fromNow()} by{' '}
+                        {link.postedBy.name}
+                      </span>
+                    </div>
                   </div>
                   <div>
-                    <span>
-                      {moment(link.createdAt).fromNow()} by {link.postedBy.name}
-                    </span>
-                  </div>
-                  <div>
-                    <span>
+                    <span className='md:mr-5'>
                       {link.type} / {link.medium}
                     </span>
                     {link.categories.map((category, index) => {
-                      return <span key={index}>{category.name}</span>;
+                      return (
+                        <span key={index} className='mr-3'>
+                          {category.name}
+                        </span>
+                      );
                     })}
                   </div>
                 </div>
